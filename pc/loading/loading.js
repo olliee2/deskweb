a83775058d376b703866da8dfc747d84648b0940
olliee2 <218727432+olliee2@users.noreply.github.com>
2025-07-14 09:17:11 +0100

    Refactor code formatting; adjust indentation in HTML and JavaScript files for consistency

--- pc/loading/loading.js
+++ pc/loading/loading.js
@@ -1,21 +1,22 @@
 import { redirect } from '../common/utils.js';
+
 const params = new URLSearchParams(document.location.search);
 const destination = params.get('destination');
 if (!destination)
-    throw new Error('Missing destination');
+  throw new Error('Missing destination');
 const duration = Number(params.get('duration'));
 if (!duration)
-    throw new Error('Missing duration');
+  throw new Error('Missing duration');
 const dotsSpan = document.getElementById('dots');
 if (!dotsSpan)
-    throw new Error('Missing dotsSpan');
+  throw new Error('Missing dotsSpan');
 setTimeout(() => {
-    redirect(destination);
+  redirect(destination);
 }, duration);
 let dots = 0;
 setInterval(() => {
-    dots++;
-    if (dots > 3)
-        dots = 0;
-    dotsSpan.textContent = '.'.repeat(dots);
+  dots++;
+  if (dots > 3)
+    dots = 0;
+  dotsSpan.textContent = '.'.repeat(dots);
 }, 400);
