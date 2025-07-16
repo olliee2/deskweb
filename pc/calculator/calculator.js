d72b3c622fe372f184df645d7f22e258b6637873
olliee2 <218727432+olliee2@users.noreply.github.com>
2025-07-16 08:41:14 +0100

    Add event listeners for equals, backspace, and keyboard input; implement equation calculation

--- pc/calculator/calculator.js
+++ pc/calculator/calculator.js
@@ -21,5 +21,46 @@ for (const button of characterButtons) {
     button.addEventListener('click', () => {
         equation += symbol;
         equationDiv.textContent = equation;
+        answerDiv.textContent = '';
     });
+}
+const equalsButton = document.getElementById('equals');
+if (!(equalsButton instanceof HTMLElement))
+    throw new Error('Missing equals');
+const backspaceButton = document.getElementById('backspace');
+if (!(backspaceButton instanceof HTMLElement))
+    throw new Error('Missing backspace');
+const clearButton = document.getElementById('clear');
+if (!(clearButton instanceof HTMLElement))
+    throw new Error('Missing clear');
+equalsButton.addEventListener('click', () => {
+    answerDiv.textContent = calculateEquation(equation);
+});
+backspaceButton.addEventListener('click', () => {
+    equation = equation.slice(0, -1);
+    equationDiv.textContent = equation;
+    answerDiv.textContent = '';
+});
+document.addEventListener('keydown', (ev) => {
+    const key = ev.key;
+    console.log(key);
+    if (symbols.includes(key)) {
+        ev.preventDefault();
+        equation += key;
+        equationDiv.textContent = equation;
+    }
+    else if (key === '/') {
+        equation += '÷';
+        equationDiv.textContent = equation;
+    }
+    else if (key === 'Enter' || key === '=') {
+        answerDiv.textContent = calculateEquation(equation);
+    }
+});
+function calculateEquation(equation) {
+    console.log(equation);
+    const exampleEquation = '(1+1)/3';
+    if (!equationValid(exampleEquation)) {
+    }
+    return 4;
 }
