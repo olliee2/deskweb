86c5e4f8f962d3b8791321f89e8d47c0fbee5ed9
olliee2 <218727432+olliee2@users.noreply.github.com>
2025-07-21 18:37:56 +0100

    Enhance Todos feature with improved UI layout and functionality; add event listener for adding todos

--- pc/todos/todos.ts
+++ pc/todos/todos.ts
@@ -1,1 +1,21 @@
-console.log('loaded');
+const todoInput = document.getElementById('todo');
+if (!(todoInput instanceof HTMLInputElement)) throw new Error('Missing todo');
+const submitButton = document.getElementById('submit');
+if (!submitButton) throw new Error('Missing submit');
+const todos = document.getElementById('todos');
+if (!todos) throw new Error('Missing todos');
+
+todoInput.value = '';
+
+submitButton.addEventListener('click', () => {
+  const todoMessage = todoInput.value;
+  if (!todoMessage) return;
+
+  todoInput.value = '';
+
+  const li = document.createElement('li');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+
+  li.append(checkbox);
+});
